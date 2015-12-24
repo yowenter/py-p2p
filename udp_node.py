be9309c7c67097b6b164248a591d8eacb541e3f2
wenter.wu <wenter.wu@daocloud.io>
2015-12-24 17:34:22 +0800

    add local ip

--- udp_node.py
+++ udp_node.py
@@ -2,8 +2,8 @@ import socket
 import os
 import json
 import time
-SERVER_IP=os.getenv("SERVER_IP","107.170.255.192")
-SERVER_PORT=os.getenv("SERVER_PORT",8001)
+SERVER_IP=os.getenv("SERVER_IP")
+SERVER_PORT=os.getenv("SERVER_PORT")
 
 
 class Node:
@@ -11,31 +11,44 @@ class Node:
         self._socket= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._broker_address=(broker_ip,broker_port)
         self._socket.bind(('',local_port))
+        self._public_address=None
+        self._local_address=(self._get_ip(),local_port)
+        self._connect_another_node=False
+        
+    def _get_ip(self):
+        public_host=socket.gethostbyname('github.com')
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((public_host,80))
+        return s.getsockname()[0]
    
         
     
     def connect_server(self):
         while True:
-#             self._socket.connect(self._broker_address)
-            self._socket.sendto(json.dumps({"from":"node","data":"ping"}),self._broker_address)
-            self._local_address=self._socket.getsockname()
-            self._public_address=None
-            self._socket.sendto(json.dumps({"from":"node","private":self._local_address}),self._broker_address)
+            if not self._connect_another_node:
+                self._socket.sendto(json.dumps({"from":"node","data":"ping"}),self._broker_address)
+                
+                self._socket.sendto(json.dumps({"from":"node","private":self._local_address}),self._broker_address)
+            
             data,address=self._socket.recvfrom(2048)
             if address[0]==self._broker_address[0]:
+                print "received from server",data
                 try:
                     data=json.loads(data)
                 except:
                     print "data not json "
                     
                 if isinstance(data,dict):
-                    if data.get("your_address"):
+                    if data.get("your_address") and not self._public_address:
                         self._public_address=tuple(data['your_address'])
 
                     if data.get("nodes"):
                         if len(data['nodes'])>1:
                             print "nodes more than one",data['nodes']
                             for n in data['nodes']:
+                                if not self._public_address:
+                                    print "self public address not fetch."
+                                    continue
                                 if str(n['public'])!=str(list(self._public_address)):
                                     self._socket.sendto("Nice to meet U!- %s:%s"%(str(self._local_address),str(self._public_address)),tuple(n['public']))
                                     self._socket.sendto("Nice to meet U!- %s:%s"%(str(self._local_address),str(self._public_address)),tuple(n['private']))                                        
@@ -54,9 +67,10 @@ class Node:
                     time.sleep(3)
                     data,address=self._socket.recvfrom(1024)
                     print "received ",data,address
+                    self._connect_another_node=True
 
 if __name__=='__main__':
-    n=Node(SERVER_IP,SERVER_PORT,9102)   
+    n=Node(SERVER_IP,SERVER_PORT,7082)   
     n.connect_server()       
                     
             
