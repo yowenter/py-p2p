cbce3043340dfb375b22139e6144673fec96c427
wenter.wu <wenter.wu@daocloud.io>
2015-12-25 18:19:18 +0800

    fix

--- udp_node.py
+++ udp_node.py
@@ -66,7 +66,7 @@ class Node:
                                 
                                 if str(n['public'])!=str(list(self._public_address)):
                                     self._socket.sendto("To %s,\nNice to meet U! \n-From %s:%s"%(str(address),str(self._local_address),str(self._public_address)),tuple(n['public']))
-                                if n['private'] and n['private']!=str(list(self._local_address)): 
+                                if n['private'] and str(n['private'])!=str(list(self._local_address)): 
                                     self._socket.sendto("To %s,\nNice to meet U! \n-From %s:%s"%(str(address),str(self._local_address),str(self._public_address)),tuple(n['private']))        
                                                                     
                                                 
